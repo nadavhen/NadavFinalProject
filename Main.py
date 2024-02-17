4b5b7b049fd9b0eab6053b12f6fd186b321ef825
Nadav Hen <nadavhen@gmail.com>
2024-02-17 17:23:49 +0200

    Main program

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
 import GUI as gui
+from Bio.Seq import Seq
+
 
 res=gui.run_gui()
 if res[0]=="file":
@@ -10,4 +12,28 @@ elif res[0]=="text":
 seq_length=len(sequence)
 parameters=gui.para_gui(seq_length)
 
-#
+#breaking parameters dictionary into variables
+length=parameters["length"]
+start=parameters["start"]
+end=parameters["end"]
+gc=parameters["gc"]
+tm=parameters["tm"]
+dg=parameters["dg"]
+amplicon_option=parameters["amplicon_option"]
+
+#defining the amplicon
+if amplicon_option==1:
+    start=0
+    end=length-1
+    amplicon=sequence[start:end]
+elif amplicon_option==2:
+    amplicon=sequence[start-1:end-1]
+elif amplicon_option==3:
+    start=0
+    end=seq_length-1
+    amplicon=sequence[start:end]
+    
+
+
+
+
