9de9467e31b0e80265b0806ac7097b5be1739bcd
Nadav Hen <nadavhen@gmail.com>
2024-02-23 21:39:30 +0200

    All graphic user interfaces used by the program

--- GUI.py
+++ GUI.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
+import pandas as pd
 
 def run_gui():
     root = tk.Tk()
@@ -9,7 +10,7 @@ def run_gui():
     # Initialize return_value as an attribute of root to ensure it is accessible outside nested functions
     root.return_value = None
 
-    def validate_input(event):
+    def validate_input(event=None):
         input_text = text_box.get("1.0", "end-1c")
         if all(char in "gtacGTAC" for char in input_text):
             sub_button.grid(row=2, column=1, sticky='W')
@@ -18,7 +19,11 @@ def run_gui():
             label_invalid.grid(row=2, column=1, sticky="W")
             sub_button.grid_forget()
 
-    def on_select(event):
+    def on_select(event=None):
+        # Using after to make sure the UI updates are scheduled and processed in the event loop
+        root.after(100, update_layout)
+
+    def update_layout():
         selected_option = dropdown.get()
         if selected_option == "raw text":
             label_tb.grid(row=1, column=0, sticky="N")
@@ -26,9 +31,9 @@ def run_gui():
             browse_button.grid_forget()
             label_browse.grid_forget()
             path_label.grid_forget()
-            sub_button.grid(row=2, column=1, sticky='W')
+            validate_input()  # Call validate to correctly place submit button
             root.geometry("500x200")
-        elif selected_option == "FASTA or Genebank file":
+        elif selected_option == "FASTA file":
             label_browse.grid(row=1, column=0)
             browse_button.grid(row=1, column=1)
             text_box.grid_forget()
@@ -37,23 +42,22 @@ def run_gui():
             root.geometry("350x100")
 
     def browse_file():
-        file_path = filedialog.askopenfilename()
+        file_path = filedialog.askopenfilename(filetypes=(("FASTA files", "*.fasta"),("FASTA files", "*.fa"), ("All files", "*.*")))
         if file_path:  # Ensure a file was selected
-            print("Selected file:", file_path)
             path_label.config(text=file_path)
             path_label.grid(row=2, column=1, sticky="W")
             sub_button.grid(row=3, column=1, sticky='W')
-
+            
     def submit():
         selected_option = dropdown.get()
         if selected_option == "raw text":
             root.return_value = ['text', text_box.get("1.0", "end-1c")]
-        elif selected_option == "FASTA or Genebank file":
+        elif selected_option == "FASTA file":
             root.return_value = ['file', path_label.cget("text")]
         root.destroy()
 
     # GUI layout and configuration
-    dropdown = ttk.Combobox(root, values=["-select input type-", "raw text", "FASTA or Genebank file"])
+    dropdown = ttk.Combobox(root, values=["-select input type-", "raw text", "FASTA file"], state="readonly")
     dropdown.current(0)  # Set the default selected option
     dropdown.grid(row=0, column=1, sticky="w")
     dropdown.bind("<<ComboboxSelected>>", on_select)
@@ -77,6 +81,8 @@ def run_gui():
 
     # Return the value set on root object after the GUI is closed
     return root.return_value
+
+
 
 def para_gui(length_default):
     def on_validate(P):
@@ -120,16 +126,20 @@ def para_gui(length_default):
 
     def toggle_dg():
         dg_spinbox.config(state='normal' if activate_dg_var.get() else 'disabled')
+    
+    def toggle_pl():
+        pl_spinbox.config(state='normal' if activate_pl_var.get() else 'disabled')
 
     def submit_and_close():
         """Collect input values, store in a dict, and close the window."""
-        result['length'] = length_entry.get() if radio_value.get() == 1 else None
-        result['range_start'] = start_entry.get() if radio_value.get() == 2 else None
-        result['range_end'] = end_entry.get() if radio_value.get() == 2 else None
+        result['length'] = int(length_entry.get()) if radio_value.get() == 1 else 0
+        result['start'] = int(start_entry.get()) if radio_value.get() == 2 else 0
+        result['end'] = int(end_entry.get()) if radio_value.get() == 2 else 0
         result['amplicon_option'] = radio_value.get()
-        result['gc'] = gc_spinbox.get() if activate_gc_var.get() else None
-        result['tm'] = tm_spinbox.get() if activate_tm_var.get() else None
-        result['dg'] = dg_spinbox.get() if activate_dg_var.get() else None
+        result['pl'] = int(pl_spinbox.get()) if activate_pl_var.get() else 20
+        result['gc'] = int(gc_spinbox.get()) if activate_gc_var.get() else 40
+        result['tm'] = int(tm_spinbox.get()) if activate_tm_var.get() else 55
+        result['dg'] = int(dg_spinbox.get()) if activate_dg_var.get() else -10
         root.destroy()
 
     root = tk.Tk()
@@ -150,6 +160,7 @@ def para_gui(length_default):
     activate_gc_var = tk.BooleanVar(value=False)
     activate_tm_var = tk.BooleanVar(value=False)
     activate_dg_var = tk.BooleanVar(value=False)
+    activate_pl_var = tk.BooleanVar(value=False)
 
     # Amplicon Placement
     amplicon_placement_label = tk.Label(root, text="Amplicon Placement", font=('Arial', 12, 'bold'))
@@ -185,6 +196,16 @@ def para_gui(length_default):
     # Other Parameters
     other_parameters_label = tk.Label(root, text="Other Parameters", font=('Arial', 12, 'bold'))
     other_parameters_label.pack(fill='x', **padding)
+    
+    # Primer Length
+    pl_frame = tk.Frame(root)
+    pl_frame.pack(fill='x', **padding)
+    activate_pl_checkbtn = tk.Checkbutton(pl_frame, text="", variable=activate_pl_var, onvalue=True, offvalue=False, command=toggle_pl, font=font)
+    pl_label = tk.Label(pl_frame, text="Primer\nLenght:", font=font)
+    pl_spinbox = tk.Spinbox(pl_frame, from_=15, to=50, textvariable=tk.DoubleVar(value=20), width=spinbox_width, font=font, state='disabled')
+    activate_pl_checkbtn.pack(side=tk.LEFT, **padding)
+    pl_label.pack(side=tk.LEFT, **padding)
+    pl_spinbox.pack(side=tk.LEFT, **padding)
 
     # GC%
     gc_frame = tk.Frame(root)
@@ -210,7 +231,7 @@ def para_gui(length_default):
     dg_frame = tk.Frame(root)
     dg_frame.pack(fill='x', **padding)
     activate_dg_checkbtn = tk.Checkbutton(dg_frame, text="", variable=activate_dg_var, onvalue=True, offvalue=False, command=toggle_dg, font=font)
-    dg_label = tk.Label(dg_frame, text="Maximal\nΔG:", font=font)
+    dg_label = tk.Label(dg_frame, text="Minimal\nΔG:", font=font)
     dg_spinbox = tk.Spinbox(dg_frame, from_=-100, to=0, increment=0.1, textvariable=tk.DoubleVar(value=-10), width=spinbox_width, font=font, state='disabled')
     activate_dg_checkbtn.pack(side=tk.LEFT, **padding)
     dg_label.pack(side=tk.LEFT, **padding)
@@ -228,3 +249,55 @@ def para_gui(length_default):
     return result  # Return the collected input values
 
 
+def displayAndSave_gui(df: pd.DataFrame):
+    def save_file():
+        file_name = filedialog.asksaveasfilename(defaultextension=".txt",
+                                                filetypes=[("Text documents", "*.txt"), ("All files", "*.*")])
+        if file_name:
+            # Assuming saving the DataFrame content
+            df.to_csv(file_name, index=False)
+            status_label.config(text="Saved!", fg="green")
+            status_label.pack(pady=5)
+
+    def display_dataframe(df):
+        for i in tree.get_children():
+            tree.delete(i)
+        tree["columns"] = list(df.columns)
+        tree.column("#0", width=0, stretch=tk.NO)
+        for col in df.columns:
+            col_width = max(df[col].astype(str).map(len).max(), len(col)) * 10
+            tree.column(col, anchor=tk.CENTER, width=col_width)  # Center column values
+            tree.heading(col, text=col, anchor=tk.CENTER)  # Center column headings
+        for row in df.itertuples(index=False, name=None):
+            tree.insert("", tk.END, values=row)  # Insert row values
+
+
+    # Create the main window
+    root = tk.Tk()
+    root.title("Display DataFrame and Save")
+
+    # Styling for the Treeview
+    style = ttk.Style()
+    style.configure("Treeview", rowheight=25, background="#D3D3D3",
+                    fieldbackground="#D3D3D3", foreground="black")
+    style.configure("Treeview.Heading", font=('Calibri', 13, 'bold'), background="lightblue")
+    style.map('Treeview', background=[('selected', '#A9A9A9')])  # Change selection color to dark gray
+
+    # Adjust alignment and background for cells
+    style.configure("Treeview", rowheight=30)  # Adjust row height for better visibility
+    style.layout("Treeview", [('Treeview.treearea', {'sticky': 'nswe'})])  # Layout adjustment
+
+    # Create and pack the Treeview widget
+    tree = ttk.Treeview(root)
+    display_dataframe(df)
+    tree.pack(pady=20, fill='x', expand=True)
+
+    # Create a 'Save' button
+    save_button = tk.Button(root, text="Save", command=save_file)
+    save_button.pack(pady=5)
+
+    # Create a status label (initially hidden or empty)
+    status_label = tk.Label(root, text="", fg="green")
+
+    # Start the GUI event loop
+    root.mainloop()
